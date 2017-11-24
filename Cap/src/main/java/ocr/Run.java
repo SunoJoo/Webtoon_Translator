0ceead505d4df4b9f0c720be26d6de72885631e4
SunoJoo <32032718+SunoJoo@users.noreply.github.com>
2017-11-25 04:44:03 +0900

    ff
    dd

--- Cap/src/main/java/ocr/Run.java
+++ Cap/src/main/java/ocr/Run.java
@@ -13,22 +13,24 @@ public class Run {
 	 */
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//String path1 = "./src/main/resources/webtoon_img/";
-		//ImageProcessing ip = new ImageProcessing(path1);
-		//ip.start(path1);
-		//new Crawler();		
-		Detect detect = new Detect();
+		/**
+		 * @param args
+		 * Crawler는 스레드가 따로 돌기 때문에 join해줘야함
+		 * 아직 join을 안해줬으므로 Crawler먼저 main에서 실행 후 주석처리하고 Detect부터 실행하면 됨
+		 * 구글 API는 각자 받은 json파일과 본인 계정을 사용하기 바람(제거는 이미 많이씀..)
+		 */		
+			// TODO Auto-generated method stub
 		
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 =======
 >>>>>>> parent of a36e567... Revert "32"
-<<<<<<< HEAD
 =======
->>>>>>> parent of 3747e7f... Merge branch 'master' of
+>>>>>>> parent of 4507a42... rollback
+<<<<<<< HEAD
 		String path = "./src/main/resources/webtoon_img/_13.jpg";
 <<<<<<< HEAD
 =======
@@ -52,7 +54,6 @@ public class Run {
 =======
 >>>>>>> parent of a36e567... Revert "32"
 			detect.detectText(path, p);
-<<<<<<< HEAD
 =======
 			
 			new Crawler("http://comic.naver.com/webtoon/detail.nhn?titleId=679519&no=147&weekday=mon");		
@@ -82,6 +83,7 @@ public class Run {
 =======
 			detect.detectText(path, p);		
 >>>>>>> parent of c90041f... 32
+<<<<<<< HEAD
 =======
 >>>>>>> parent of 3747e7f... Merge branch 'master' of
 =======
@@ -89,7 +91,10 @@ public class Run {
 >>>>>>> parent of c90041f... 32
 =======
 >>>>>>> parent of a36e567... Revert "32"
+=======
+>>>>>>> parent of 4507a42... rollback
 				
+<<<<<<< HEAD
 			coordinator = new Coordinator(detect.getDes(),detect.getCoord());
 			coordinator.coordinate();
 			calculator = new Calculator(coordinator.getDes(),coordinator.getCoordParent());
@@ -99,11 +104,44 @@ public class Run {
 				
 				System.out.println(calculator.getSentenceIndex().get(i));				
 			}
+=======
+				
+				for(int i = 0 ; i<calculator.getSentenceArray().length; i++) {
+					System.out.println(calculator.getSentenceArray()[i]);
+					System.out.println(calculator.getSentenceIndex().get(i));
+				}
+				
+				
+				
+				for(int i = 0; i<calculator.getSentenceArray().length;i++) {				
+					Trans translator = new Trans();
+					result.addText(translator.translator(calculator.getSentenceArray()[i]));				
+				}
+				result.setPosition(calculator.getSentenceIndex());		
+
+				for(int i = 0; i<result.getText().size();i++) {
+					System.out.println(result.getText().get(i));
+					System.out.println(result.getPosition().get(i));
+				}
+				System.out.println(result.getPosition().size());
+				System.out.println(result.getPosition().get(0).size());
+				System.out.println(result.getText().size());
+				InputText id = new InputText(result);
+				id.start();
+					
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+>>>>>>> branch 'master' of https://github.com/SunoJoo/Webtoon_Translator.git
 			
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 4507a42... rollback
 <<<<<<< HEAD
 =======
 =======
@@ -117,6 +155,7 @@ public class Run {
 			
 <<<<<<< HEAD
 >>>>>>> parent of c90041f... 32
+<<<<<<< HEAD
 =======
 >>>>>>> parent of 3747e7f... Merge branch 'master' of
 =======
@@ -124,11 +163,12 @@ public class Run {
 =======
 <<<<<<< HEAD
 >>>>>>> parent of a36e567... Revert "32"
+=======
+>>>>>>> parent of 4507a42... rollback
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+=======
+>>>>>>> branch 'master' of https://github.com/SunoJoo/Webtoon_Translator.git
 		}
-		
-		
-	}
 }
---
+++ Cap/src/main/java/web/connect/CommandIf.java
@@ -0,0 +1,9 @@
+package web.connect;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface CommandIf {
+	public Object processCommand(HttpServletRequest req, HttpServletResponse resp);
+}
+
