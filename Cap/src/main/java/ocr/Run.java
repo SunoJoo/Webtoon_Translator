4823e28348a05a80374ff50ad681b3e74f1a2acf
SunoJoo <32032718+SunoJoo@users.noreply.github.com>
2017-11-25 04:43:44 +0900

    ff
    ff

--- Cap/src/main/java/ocr/Run.java
+++ Cap/src/main/java/ocr/Run.java
@@ -1,19 +1,17 @@
 package ocr;
 
-
 import java.io.File;
 import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class Run {
-	
-	
 	/**
 	 * @param args
 	 * Crawler는 스레드가 따로 돌기 때문에 join해줘야함
 	 * 아직 join을 안해줬으므로 Crawler먼저 main에서 실행 후 주석처리하고 Detect부터 실행하면 됨
 	 * 구글 API는 각자 받은 json파일과 본인 계정을 사용하기 바람(제거는 이미 많이씀..)
 	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//String path1 = "./src/main/resources/webtoon_img/";
@@ -27,23 +25,32 @@ public class Run {
 <<<<<<< HEAD
 <<<<<<< HEAD
 =======
+>>>>>>> parent of a36e567... Revert "32"
+<<<<<<< HEAD
+=======
 >>>>>>> parent of 3747e7f... Merge branch 'master' of
 		String path = "./src/main/resources/webtoon_img/_13.jpg";
+<<<<<<< HEAD
 =======
 		String path = "./src/main/resources/webtoon_img/_42.jpg";
 >>>>>>> parent of c90041f... 32
 =======
 		String path = "./src/main/resources/webtoon_img/_42.jpg";
 >>>>>>> parent of c90041f... 32
+=======
+>>>>>>> parent of a36e567... Revert "32"
 		File f = new File("./text/lines.txt");
 		PrintStream p;	
 		Coordinator coordinator;
 		Calculator calculator;		
 		
-		try {		
+		try {
 			p = new PrintStream(f);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of a36e567... Revert "32"
 			detect.detectText(path, p);
 <<<<<<< HEAD
 =======
@@ -71,6 +78,7 @@ public class Run {
 				calculator = new Calculator(coordinator.getDes(),coordinator.getCoordParent());
 				calculator.calResult();		
 >>>>>>> branch 'master' of https://github.com/SunoJoo/Webtoon_Translator.git
+<<<<<<< HEAD
 =======
 			detect.detectText(path, p);		
 >>>>>>> parent of c90041f... 32
@@ -79,16 +87,20 @@ public class Run {
 =======
 			detect.detectText(path, p);		
 >>>>>>> parent of c90041f... 32
+=======
+>>>>>>> parent of a36e567... Revert "32"
 				
 			coordinator = new Coordinator(detect.getDes(),detect.getCoord());
 			coordinator.coordinate();
 			calculator = new Calculator(coordinator.getDes(),coordinator.getCoordParent());
 			calculator.calresult();
-			for(int i = 0; i<calculator.getSentenceArray().length;i++) {				
+			for(int i = 0; i<calculator.getSentenceArray().length;i++) {
 				System.out.println(calculator.getSentenceArray()[i]);
-				System.out.println(calculator.getSentenceIndex().get(i));						
+				
+				System.out.println(calculator.getSentenceIndex().get(i));				
 			}
 			
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -109,6 +121,9 @@ public class Run {
 >>>>>>> parent of 3747e7f... Merge branch 'master' of
 =======
 >>>>>>> parent of c90041f... 32
+=======
+<<<<<<< HEAD
+>>>>>>> parent of a36e567... Revert "32"
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
